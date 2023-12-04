22bab5e356509846ea7677c4fbd384f2b92f1406
Liam Bush <liambush@Liams-MacBook-Pro-3.local>
2023-12-03 19:17:20 -0500

    Initial commit

--- token_31924/deploy.sh
+++ token_31924/deploy.sh
@@ -2,4 +2,4 @@ PRIVATEKEY="APrivateKey1zkp7oJ1kFkz8897R7ARuGh7su8sc4Szg4AdyqbZehMAEpg3"
 
 APPNAME="token_31924"
 
-cd .. && snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast" --fee 1000000
+cd .. && snarkos developer deploy "${APPNAME}.aleo" --private-key "${PRIVATEKEY}" --query "https://vm.aleo.org/api" --path "./${APPNAME}/build/" --broadcast "https://vm.aleo.org/api/testnet3/transaction/broadcast"
